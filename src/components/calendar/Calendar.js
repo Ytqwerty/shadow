eb0140f9a89508bc13c00f41d4c2b2a729eba16b
Shyngys Tusupbekov <ayan_toleu@mail.ru>
2024-12-11 21:28:03 +0500

    adding components

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,12 +1,17 @@
 import React, {useState} from 'react';
 import './Calendar.css'
+import Modal from "../ui/modal/Modal";
+import Button from "../ui/button/Button";
 
 const Calendar = () => {
     const DaysOfWeek = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
     const [currentDate, setCurrentDate] = useState(new Date());
 
+    const month = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'];
     const currentYear = currentDate.getFullYear();
     const currentMonth = currentDate.getMonth();
+    const currentDay = currentDate.getDate()
 
     function getDaysInMonth(year, month) {
         return new Date(year, month + 1, 0).getDate();
@@ -17,8 +22,8 @@ const Calendar = () => {
     function getFirstDayOfMonth(year, month) {
         return new Date(year, month, 1).getDay();
     }
-
     const FirstDay = getFirstDayOfMonth(currentYear, currentMonth);
+
     const firstDay = FirstDay === 0 ? 6 : FirstDay - 1;
     const days = [];
     for (let i = 0; i < firstDay; i++) {
@@ -27,19 +32,26 @@ const Calendar = () => {
     for (let i = 1; i <= daysInMonth; i++) {
         days.push(i);
     }
+
     const rows = [];
     for (let i = 0; i < days.length; i += 7) {
         rows.push(days.slice(i, i + 7))
     }
 
-        function changeMonth(month) {
+    function changeMonth(month) {
         const newDate = new Date(currentYear, currentMonth + month, 1);
         setCurrentDate(newDate);
     }
+    const [modal, setModal] = useState(false);
 
+    function handleClick() {
+        setModal(!modal)
+    }
     return (
         <div className="calendar">
-            <div>
+            <Modal open={modal} setOpen={setModal}/>
+            <div className='year'> {month[currentMonth]} {currentYear}</div>
+            <div className='button'>
                 <button onClick={() => changeMonth(-1)}>Назад</button>
                 <button onClick={() => changeMonth(1)}>Вперед</button>
             </div>
@@ -50,9 +62,15 @@ const Calendar = () => {
                         <td key={index}>{day}</td>
                     ))}
                 </tr>
-                {days.map(function(day,index) {
-                    return(
-                        <div key={index}>{day}</div>
+                {rows.map(function (day, index) {
+                    return (
+                        <tr key={index} className='day'>
+                            {day.map(function (item, index2) {
+                                return (
+                                    <td key={index2} className={item===currentDay ? 'currentDay' : ''} onClick={handleClick}>{item}</td>
+                                )
+                            })}
+                        </tr>
                     )
                 })}
                 </thead>
